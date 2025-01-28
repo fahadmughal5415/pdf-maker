b778adee41d040edafec0d2dc773f60f14df0d7b
Programming Point <fahadmughal5415@gmail.com>
2025-01-28 12:52:53 +0500

    Increase font size in PDF output and adjust line position

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 for index, row in df.iterrows():
     pdf.add_page()
-    pdf.set_font(family="Times", style="B", size=12)
+    pdf.set_font(family="Times", style="B", size=24)
     pdf.set_text_color(100,100,100)
-    pdf.line(10, 19, 200, 19)
+    pdf.line(10, 20, 200, 20)
     pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1, border=0)
 
 pdf.output("output.pdf")
